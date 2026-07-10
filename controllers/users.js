8c8065d3c07bbc829eec49cdb810c08aaa1f2bc4
S Madhavan <193496449+smadhavan1@users.noreply.github.com>
2026-07-10 20:50:04 +0530

    Add additional charts to the dashboard

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,9 @@ import setToastAndRedirect from "../helpers/setToast.js";
 
 import difficultyLevels from "../constants/difficultyLevels.js";
 import Question from "../models/question.js";
+import platforms from "../constants/platforms.js";
+import tagList from "../constants/tags.js";
+import statusOptions from "../constants/statusOptions.js";
 
 const registerForm = (req, res) => {
 	if (req.isAuthenticated()) {
@@ -72,9 +75,91 @@ const dashboard = async (req, res) => {
 	};
 	const difficultyConfig = {
 		type: "doughnut",
-		data: difficultyData
+		data: difficultyData,
+		options: { plugins: { legend: { labels: { boxWidth: 25 } } } }
 	};
-	res.render("users/dashboard", { difficultyConfig });
+
+	const statusPipeline = [{ $match: { owner: req.user._id } }, { $group: { _id: "$status", count: { $count: {} } } }, { $addFields: { __order: { $indexOfArray: [statusOptions, "$_id"] } } }, { $sort: { __order: 1 } }];
+	const statusCounts = await Question.aggregate(statusPipeline);
+	const statusData = {
+		labels: statusCounts.map((statusCount) => {
+			return statusCount._id;
+		}),
+		datasets: [
+			{
+				data: statusCounts.map((statusCount) => {
+					return statusCount.count;
+				}),
+				backgroundColor: ["rgb(99, 255, 125)", "rgb(235, 193, 54)", "rgb(255, 86, 142)"],
+				hoverOffset: 4
+			}
+		]
+	};
+	const statusConfig = {
+		type: "doughnut",
+		data: statusData,
+		options: { plugins: { legend: { labels: { boxWidth: 25 } } } }
+	};
+
+	const platformPipeline = [{ $match: { owner: req.user._id } }, { $group: { _id: "$platform", count: { $count: {} } } }, { $addFields: { __order: { $indexOfArray: [platforms, "$_id"] } } }, { $sort: { __order: 1 } }];
+	const platformCounts = await Question.aggregate(platformPipeline);
+
+	const platformData = {
+		labels: platformCounts.map((platformCount) => {
+			return platformCount._id;
+		}),
+		datasets: [
+			{
+				data: platformCounts.map((platformCount) => {
+					return platformCount.count;
+				}),
+				backgroundColor: "rgba(54, 162, 235, 0.2)",
+				borderColor: "rgb(54, 162, 235)",
+				borderWidth: 1,
+				categoryPercentage: 0.5,
+				barPercentage: 0.5,
+				hoverOffset: 4
+			}
+		]
+	};
+	const platformConfig = {
+		type: "bar",
+		data: platformData,
+		options: {
+			indexAxis: "x",
+			plugins: { legend: { display: false } }
+		}
+	};
+
+	const topicPipeline = [{ $match: { owner: req.user._id } }, { $unwind: "$tags" }, { $group: { _id: "$tags", count: { $count: {} } } }, { $sort: { count: -1 } }];
+	const topicCounts = await Question.aggregate(topicPipeline);
+
+	const topicData = {
+		labels: topicCounts.map((topicCount) => {
+			return topicCount._id;
+		}),
+		datasets: [
+			{
+				data: topicCounts.map((topicCount) => {
+					return topicCount.count;
+				}),
+				backgroundColor: "rgba(75, 192, 192, 0.2)",
+				borderColor: "rgb(75, 192, 192)",
+				borderWidth: 1,
+				hoverOffset: 4
+			}
+		]
+	};
+	const topicConfig = {
+		type: "bar",
+		data: topicData,
+		options: {
+			indexAxis: "y",
+			plugins: { legend: { display: false } },
+			scales: { x: { ticks: { precision: 0 } } }
+		}
+	};
+	res.render("users/dashboard", { difficultyConfig, platformConfig, topicConfig, statusConfig });
 };
 
 export { registerForm, register, loginForm, login, logout, dashboard };
